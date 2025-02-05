c56d00161a6df1152ae8a8998c0e60b46ee5b3ad
Emmanuel Obua <emmanuelo@intanode.co.ug>
2025-02-05 12:07:26 +0300

    mapped relationships

--- src/Models/District.php
+++ src/Models/District.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class District extends Model
 {
-	protected $fillable = ['name'];
+	protected $fillable = ['name','region_id'];
+
+	public function region()
+	{
+	    return $this->belongsTo(Region::class);
+	}
 
 	public function counties()
 	{
